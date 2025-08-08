040b1179447c193c675801a1edda4eb906164a5f
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-08-08 16:45:50 +0800

    optimized

--- main.js
+++ main.js
@@ -170,7 +170,12 @@ canvas.addEventListener('click', (event) => {
 
 let hoveredSprite = null;
 
+let lastMove = 0;
 canvas.addEventListener('mousemove', (event) => {
+  const now = performance.now()
+  if (now - lastMove < 50) return
+  lastMove = now
+  
   const rect = canvas.getBoundingClientRect();
   mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
   mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
